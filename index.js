082769cac684e06fb22b3e13d321ee468a0978d1
Mike Barber <bandofgold1@gmail.com>
2023-09-27 16:23:31 -0400

    added shape object and passed in values

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
 
 //main function
 async function generateLogo(){
