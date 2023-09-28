89438b255b90548252efb4bc7222f4ffedefd2cb
Mike Barber <bandofgold1@gmail.com>
2023-09-28 16:27:08 -0400

    shape generation and bug fixes
    fixed input section, added shape generation

--- index.js
+++ index.js
@@ -1,59 +1,114 @@
-const inquirer = require('inquirer');
+//dependencies
 const fs = require('fs');
 
+const inquirer = require('inquirer');
 const Circle = require('./lib/circle');
 const Triangle = require('./lib/triangle');
 const Square = require('./lib/square');
 
-//main function
-async function generateLogo(){
-    const ColorOptions = [
-        'black',
-        'gray',
-        'white',
-        'brown',
-        'red',
-        'orange',
-        'yellow',
-        'green',
-        'blue',
-        'purple',
-        'pink'
+// function to generate logo
+async function generateLogo() {
+    try {
+        //choice arrays
+        const shapeChoices = ['Circle', 'Square', 'Triangle'];
+        const colorChoices = [
+            'black',
+            'gray',
+            'white',
+            'brown',
+            'red',
+            'orange',
+            'yellow',
+            'green',
+            'blue',
+            'yellow',
+            'orange',
+            'purple',
+            'pink'
     ];
-    const shapeOptions = [
-        'circle', 
-        'triangle', 
-        'square'
-    ];
-
-const input = await inquirer.prompt([
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Please enter a maximum of three characters for the logo text:',
-        validate: (text) => text.length <= 3,
-    },
-    {
-        type: 'list',
-        name: 'textColor',
-        message: 'Please select a text color:',
-        choices: ColorOptions,
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Please select a shape:',
-        choices: shapeOptions,
-    },
-    {
-        type: 'list',
-        name: 'logoColor',
-        message: 'Please select a logo color:',
-        choices: ColorOptions,
-    },
-    
+//input section                
+    // text input
+    const { text } = await inquirer.prompt([
+        {
+            name: 'text',
+            type: 'input',
+            message: 'Enter up to 3 letters to appear on your logo:',
+            validate: (input) => {
+                if (input.length > 3) {
+                    return 'Please only enter up to 3 letters';
+                }
+            return true;
+          },
+        },
     ]);
-    
-}
-//call main function
-generateLogo();
+  
+    // text color input
+    const { textColor } = await inquirer.prompt([
+        {
+            name: 'textColor',
+            type: 'list',
+            message: 'Select a color for your Text:',
+            choices: colorChoices,
+
+        },
+    ]);
+  
+    // shape type input
+    const { shape } = await inquirer.prompt([
+        {
+            name: 'shape',
+            type: 'list',
+            message: 'Select a shape for your logo:',
+            choices: shapeChoices,
+        },
+      ]);
+  
+      // logo color input
+      const { shapeColor } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'shapeColor',
+            message: 'Select a color for your logo:',
+            choices: colorChoices,
+        },
+      ]);
+//section to create logo
+    //shape object
+    let logoShape;
+    switch (shape) {
+    case 'Circle':
+        logoShape = new Circle();
+        break;
+    case 'Triangle':
+        logoShape = new Triangle();
+        break;
+    case 'Square':
+        logoShape = new Square();
+        break;
+    default:
+        throw new Error('error invalid shape');
+    }
+
+
+      // Sets objects parameters, then render SVG string
+      logoShape.setText(text);
+      logoShape.setTextColor(textColor);
+      logoShape.setColor(shapeColor);
+
+      //call render and make svgFile to write
+      const svgFile = logoShape.outputRender();
+  
+      // write svgFile
+      fs.writeFileSync('logo.svg', svgFile);
+  
+      console.log('New logo Generated');
+    } catch (error) {
+      console.error('error creating logo', error.message);
+    }
+  }
+
+
+  
+  // Call function to generate logo
+  generateLogo();
+  
